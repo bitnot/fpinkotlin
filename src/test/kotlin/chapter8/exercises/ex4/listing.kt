e1ecfb3abd45a7e7f1eeeee6d07a26c9ac51445b
bitnot <bitnot01@gmail.com>
2020-06-30 21:27:06 +0200

    Solution for C8E4

--- src/test/kotlin/chapter8/exercises/ex4/listing.kt
+++ src/test/kotlin/chapter8/exercises/ex4/listing.kt
@@ -2,11 +2,15 @@ package chapter8.exercises.ex4
 
 import chapter8.RNG
 import chapter8.State
+import chapter8.nonNegativeInt
+import chapter8.solutions.ex4.Gen
 
 data class Gen<A>(val sample: State<RNG, A>) {
     companion object {
         //tag::init[]
-        fun choose(start: Int, stopExclusive: Int): Gen<Int> = TODO()
+        fun choose(start: Int, stopExclusive: Int): Gen<Int> =
+            Gen(State { rng: RNG -> nonNegativeInt(rng) }
+                .map { it % (stopExclusive - start) + start })
         //end::init[]
     }
 }
